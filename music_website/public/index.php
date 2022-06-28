18cac6bcf8dd74baa62f3a5df967c0f0a685b4a9
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2022-06-28 16:15:38 +0900

    add: lato font

--- music_website/public/index.php
+++ music_website/public/index.php
@@ -1,10 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title>M Site</title>
-	</head>
-	<body></body>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>M Site</title>
+</head>
+
+<body>
+
+  <style>
+  @font-face {
+    src: url('assets/Lato-Regular.ttf');
+    font-family: lato;
+  }
+  </style>
+
+  <header>
+    <div class='logo-holder'>
+      <img class='logo' src="assets/images/logo.jpg" alt="">
+    </div>
+    <div class='header-div'>
+      <div class='main-title'>MUSIC WEBSITE</div>
+      <div class='main-nav'>
+        <div class="nav-item"><a href="">Home</a></div>
+        <div class="nav-item"><a href="">Music</a></div>
+        <div class="nav-item"><a href="">Category</a></div>
+        <div class="nav-item"><a href="">Artists</a></div>
+        <div class="nav-item"><a href="">About Us</a></div>
+        <div class="nav-item"><a href="">Contact Us</a></div>
+        <div class="nav-item"><a href="">Hi, user</a></div>
+      </div>
+    </div>
+  </header>
+</body>
+
 </html>
